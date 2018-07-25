50187ac7586ececf0a259788b3d94e84326d9398
Makarand Kshirsagar <mak.ksagar@gmail.com>
2018-07-25 18:00:01 +0530

    Add project files.

---
+++ WebApp_CI_CD.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApp_CI_CD.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
